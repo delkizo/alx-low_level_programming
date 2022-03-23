e13d15504d1f0c567819a6bf0b6902d23a3fd76b
delkizo <delmokaya@gmail.com>
2022-03-23 08:15:27 -0700

    cap strings and leet

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+char *rot13(char *);
+void print_number(int n);
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -7,3 +10,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+
+#endif
