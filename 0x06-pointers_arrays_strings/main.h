acb9719297416f0792fabfa5305a8122c9e53561
delkizo <delmokaya@gmail.com>
2022-03-24 12:00:01 -0700

    corrected work

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,7 @@
-char *rot13(char *);
-void print_number(int n);
-
-int _putchar(char c);
 char *_strcat(char *dest, char *src);
+void print_number(int n);
+int _putchar(char c);
+void print_buffer(char *b, int size);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -11,6 +10,4 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-void print_number(int n);
-
-#endif
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
