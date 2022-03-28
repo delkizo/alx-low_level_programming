ae2046ed5f94cd4da694f38fc898d14350b594fc
delkizo <delmokaya@gmail.com>
2022-03-28 14:11:32 -0700

    corrected work

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,28 +1,20 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
- * *_strchr - locates a character in a string
- * @s: string to search
- * @c: char to find
- *
- * Return: a pointer to the first occurrence of the character
- * c in the string s, or NULL if the character is not found
+ * _strchr - locates a character in a string
+ * @s: pointer to string
+ * @c: character to locate
+ * Return: pointer to first occurence of character c
+ * or NULL if character is not found.
  */
+
 char *_strchr(char *s, char c)
 {
-		int a;
-
-		while (1)
-		{
-			a = *s++;
-			if (a == c)
-			{
-				return (s - 1);
-			}
-			if (a == 0)
-			{
-				return (NULL);
-			}
-		}
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+else if (*(s + 1) == c)
+return (s + 1);
+s++;
+}
+return (s + 1);
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,35 +1,23 @@
-#include "main.h"
-
 /**
- * *_strspn - gets the length of a prefix substring
- * @s: string to evaluate
- * @accept: string containing the list of characters to match in s
- *
- * Return: the number of bytes in the initial segment
- * of s which consist only of bytes from accept
+ * _strspn - gets the length of a prefix substring
+ * @s: segment to compare bytes from
+ * @accept: string of bytes to compare with
+ * Return: number of bytes in segment s which consist of bytes from accept
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, f, flag;
+unsigned int i, j;
 
-	f = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		flag = 0;
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-			{
-				f++;
-				flag = 1;
-			}
-		}
-		if (flag == 0)
-		{
-			return (f);
-		}
-	}
-
-	return (0);
+i = 0;
+while (s[i] != '\0')
+{
+j = 0;
+while (accept[j] != '\0' && s[i] != accept[j])
+j++;
+if (accept[j] == '\0')
+return (i);
+i++;
+}
+return (i);
 }
